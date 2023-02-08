2dbf1789a4299258f4d5c409ccb340444ad4732e
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-02-08 14:49:53 -0500

    Publish v2023.02.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/gcp:2022.11
-docker push cimg/gcp:2022.11.1
+docker push cimg/gcp:2023.02
+docker push cimg/gcp:2023.02.1
