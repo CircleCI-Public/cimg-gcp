55e6a05dbf96cbf14c9c757d0dd44920041f785d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-02-08 15:01:40 -0500

    Publish v2023.02.1. [release] (#10)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.11/Dockerfile -t cimg/gcp:2022.11.1 -t cimg/gcp:2022.11 .
+docker build --file 2023.02/Dockerfile -t cimg/gcp:2023.02.1 -t cimg/gcp:2023.02 --platform linux/amd64 .
