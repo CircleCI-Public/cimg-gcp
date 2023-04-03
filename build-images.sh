19cf88b37f135012531ff241d6930c157d1997fe
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-04-03 18:33:27 +0100

    Publish v2023.04.1. [release] (#12)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.03/Dockerfile -t cimg/gcp:2023.03.1 -t cimg/gcp:2023.03 --platform linux/amd64 .
+docker build --file 2023.04/Dockerfile -t cimg/gcp:2023.04.1 -t cimg/gcp:2023.04 --platform linux/amd64 .
