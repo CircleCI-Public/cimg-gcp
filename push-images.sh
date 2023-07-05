7513c0a7b5555e944ed25ee40ef87ba7bd072e18
ryanbourdais <ryanbourdais@outlook.com>
2023-07-05 11:37:01 -0500

    Publish v2023.07. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/gcp:2023.06
-docker push cimg/gcp:2023.06.1
+docker push cimg/gcp:2023.07
+docker push cimg/gcp:2023.07.1
