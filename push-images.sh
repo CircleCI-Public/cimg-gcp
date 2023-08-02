a407cf6972afa0e2c1e7de244204920fc6642fd4
Adam Hartley <git@ahartley.com>
2023-08-02 15:39:36 +0100

    Publish v2023.08.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/gcp:2023.07
-docker push cimg/gcp:2023.07.1
+docker push cimg/gcp:2023.08
+docker push cimg/gcp:2023.08.1
