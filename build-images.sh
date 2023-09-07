11a47729d34f6da48145521d536ab7f469ef58ec
Adam Hartley <git@ahartley.com>
2023-09-07 11:57:32 +0100

    Publish v2023.09.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.08/Dockerfile -t cimg/gcp:2023.08.1 -t cimg/gcp:2023.08 --platform linux/amd64 .
+docker build --file 2023.09/Dockerfile -t cimg/gcp:2023.09.1 -t cimg/gcp:2023.09 --platform linux/amd64 .
