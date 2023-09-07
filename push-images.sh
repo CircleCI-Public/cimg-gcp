b1e1316bb355b24a526e7edb10dbd9b3121ce40b
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-09-07 16:25:15 +0100

    Publish v2023.09.1. [release] (#20)

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/gcp:2023.08
-docker push cimg/gcp:2023.08.1
+docker push cimg/gcp:2023.09
+docker push cimg/gcp:2023.09.1
