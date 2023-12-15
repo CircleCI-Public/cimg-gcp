fec8e76c7477298db88b2acda1673348569d6fce
Jalexchen <jeff.chen@circleci.com>
2023-12-15 11:55:34 -0800

    Publish v2023.12.1. [release] (#25)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.09/Dockerfile -t cimg/gcp:2023.09.1 -t cimg/gcp:2023.09 --platform linux/amd64 .
+docker build --file 2023.12/Dockerfile -t cimg/gcp:2023.12.1 -t cimg/gcp:2023.12 --platform linux/amd64 .
