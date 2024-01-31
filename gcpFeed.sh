a5322a128293e2ac55af48cb25137c6e9f62df30
Jalexchen <jeff.chen@circleci.com>
2024-01-31 08:09:16 -0800

    [PROV-444] feat: automate cimg/gcp (#26)

---
+++ gcpFeed.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -f shared/automated-updates.sh ]; then
+  source shared/automated-updates.sh
+else
+  echo "Check if submodule was loaded; automated-updates.sh is missing"
+  exit 1
+fi
+source ./manifest
+
+templateFile="Dockerfile.template"
+interval="monthly"
+VERSION=$( date +%Y.%m )
+
+replaceDatedTags "$templateFile" "$interval"
+releaseDeployImage "$VERSION"
