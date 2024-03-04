a40425ce6fcc0917e76921f02e136a25107299c4
Josh Bryans <joshhsoj1902@gmail.com>
2024-03-04 17:11:30 -0500

    Update cimg/deploy (#29)
    * Update cimg/deploy
    
    * Build 2024.03
    
    * Update google gpg key

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 2023.12/Dockerfile -t cimg/gcp:2023.12.1 -t cimg/gcp:2023.12 --platform linux/amd64 .
+docker build --file 2024.03/Dockerfile -t cimg/gcp:2024.03.1 -t cimg/gcp:2024.03 --platform linux/amd64 .
